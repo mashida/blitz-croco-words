111e9c299f8693bfe47c9e82d3167668cefc1f05
Nikita Serditov <34740100+mashida@users.noreply.github.com>
2024-11-01 18:56:36 +0300

    ZipArchive is parsed and we have a list of words now

--- helpers.py
+++ helpers.py
@@ -1,3 +1,36 @@
+"""
+Задача 2024.10.23.06
+
+Разархивируйте архив и достаньте один файл презентации.
+
+Откройте файл презентации в программе с помощью объекта Presentation библиотеки pptx
+"""
+import os
+from typing import IO, TextIO
+
+FILE_NAME = "Osennyaya_igra_3.pptx"
+
+from pathlib import Path
+
 from pptx import Presentation
 
-def get_files(path: )
+
+def get_words_form_file(file: IO[bytes] | TextIO) -> list[str]:
+    prs = Presentation(file)
+
+    result: list[str] = []
+
+    for slide in prs.slides:
+        for shape in slide.shapes:
+            if not shape.has_text_frame:
+                continue
+            if ' ' in shape.text or '-' in shape.text or ':' in shape.text or 'СУПЕРКРОКО' in shape.text:
+                continue
+            result.append(shape.text.strip())
+
+    return result
+
+
+if __name__ == '__main__':
+    with open('Osennyaya_igra_3.pptx', 'r') as f:
+        print(get_words_form_file(f))
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from zipfile import ZipFile
+from helpers import get_words_form_file
 
 ZIP_FILENAME = "croco-blitz-source.zip"
 
@@ -8,9 +9,14 @@ ZIP_FILENAME = "croco-blitz-source.zip"
 def read_zipped_file():
     current_folder = os.path.dirname(os.path.realpath(__file__))
 
+    words = []
+
     with ZipFile(Path(current_folder) / 'src' / ZIP_FILENAME) as archive:
         for f in archive.namelist():
-            print(f)
+            words.extend(get_words_form_file(archive.open(f)))
+
+    for index, word in enumerate(words):
+        print(f"[{index}] {word}")
 
 
 if __name__ == "__main__":
